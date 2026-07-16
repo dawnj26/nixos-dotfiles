2b4891324aeeb7846aa1b9388f24d608a664376d
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-07-16 13:55:40 +0800

    move mako startup to systemd

--- config/hypr/autostart.lua
+++ config/hypr/autostart.lua
@@ -2,6 +2,5 @@ hl.on("hyprland.start", function()
 	local utils = require("utils")
 
 	hl.exec_cmd(utils.uwsm("hyprpaper"))
-	hl.exec_cmd(utils.uwsm("mako"))
 	hl.exec_cmd(utils.uwsm("waybar"))
 end)
--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -70,4 +70,24 @@ in {
     style.name = "kvantum";
     platformTheme.name = "qtct";
   };
+
+  services.mako = {
+    enable = true;
+    settings = {
+      background-color = "#1a1b26";
+      text-color = "#a9b1d6";
+      border-color = "#0db9d7";
+      progress-color = "#0db9d7";
+      default-timeout = 5000;
+      ignore-timeout = 1;
+      layer = "overlay";
+      border-radius = 4;
+      sort = "-time";
+      font = "JetBrainsMono Nerd Font 12";
+    };
+    extraConfig = ''
+      [urgency=high]
+      border-color=#ff7a93
+    '';
+  };
 }
