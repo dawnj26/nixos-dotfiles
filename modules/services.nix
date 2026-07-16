36e99542dd9c12557a04f320cca08a22b18a3acd
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-07-16 14:00:11 +0800

    remove wayland for sddm

--- modules/services.nix
+++ modules/services.nix
@@ -4,7 +4,6 @@
 
     displayManager.sddm = {
       enable = true;
-      # wayland.enable = true;
       autoNumlock = true;
       enableHidpi = true;
       theme = "${pkgs.elegant-sddm}/share/sddm/themes/Elegant";
