2f1217db1ef07503c1bfcdf1e8f05cb4a6335f9e
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-07-16 13:56:03 +0800

    properly initialize systemd env

--- config/hypr/autostart.lua
+++ config/hypr/autostart.lua
@@ -1,6 +1,8 @@
 hl.on("hyprland.start", function()
 	local utils = require("utils")
 
+	hl.exec_cmd("systemctl --user import-environment $(env | cut -d'=' -f 1)")
+	hl.exec_cmd("dbus-update-activation-environment --systemd --all")
 	hl.exec_cmd(utils.uwsm("hyprpaper"))
 	hl.exec_cmd(utils.uwsm("waybar"))
 end)
