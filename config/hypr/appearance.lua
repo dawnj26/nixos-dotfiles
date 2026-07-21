872b9d44821b575e7dec2a4e00308a7b1a4b1b31
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-07-21 18:26:50 +0800

    increase border size and use tokyo night color for borders

--- config/hypr/appearance.lua
+++ config/hypr/appearance.lua
@@ -3,11 +3,11 @@ hl.config({
 		gaps_in = 0,
 		gaps_out = { 8, 8, 8, 8 },
 
-		border_size = 1,
+		border_size = 2,
 
 		allow_tearing = false,
 		col = {
-			active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
+			active_border = "rgb(0db9d7)",
 			inactive_border = "rgba(595959aa)",
 		},
 	},
