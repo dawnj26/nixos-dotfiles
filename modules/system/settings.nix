b4197aa93ecbc446d9833bf1e8dc7d63265d9969
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-07-24 21:07:05 +0800

    add substituters for cachyos kernel and hyprland

--- modules/system/settings.nix
+++ modules/system/settings.nix
@@ -9,4 +9,11 @@
     experimental-features = ["nix-command" "flakes"];
     auto-optimise-store = true;
   };
+
+  nix.settings = {
+    substituters = ["https://attic.xuyh0120.win/lantian" "https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hydra.nixos.org/" "https://hyprland.cachix.org"];
+    trusted-public-keys = ["lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    trusted-users = ["root" "@wheel"];
+  };
 }
