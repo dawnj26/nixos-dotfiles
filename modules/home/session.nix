69918a583468cf9070741327d87c8ecfafde7a4a
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-07-24 21:04:06 +0800

    remove xcursor env var

--- modules/home/session.nix
+++ modules/home/session.nix
@@ -2,9 +2,6 @@
   home.sessionVariables = {
     HYPRCURSOR_THEME = "Bibata-Modern-Ice";
 
-    XCURSOR_THEME = "Bibata-Modern-Ice";
-    XCURSOR_SIZE = "24";
-
     QT_QPA_PLATFORM = "wayland;xcb";
   };
 }
