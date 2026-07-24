c1a05ae48eeb74dab4b82a8b6ea2911e57696d17
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-07-24 21:06:35 +0800

    use hyprland flake

--- modules/system/hyprland.nix
+++ modules/system/hyprland.nix
@@ -1,8 +1,17 @@
-{...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: let
+  system = pkgs.stdenv.hostPlatform.system;
+in {
   programs.hyprland = {
     enable = true;
     withUWSM = true;
     xwayland.enable = true;
+    package = inputs.hyprland.packages.${system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${system}.xdg-desktop-portal-hyprland;
   };
   programs.uwsm.enable = true;
+  programs.monique.enable = true;
 }
