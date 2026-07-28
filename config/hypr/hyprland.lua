e625e517177c99cdb3e90755aef1c916139fbabf
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-07-28 21:21:18 +0800

    revert

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -1,12 +1,6 @@
 require("autostart")
 require("keybinds")
-
-local ok, _ = pcall(require, "monitors")
-
+require("monitors")
 require("rules")
 require("input")
 require("appearance")
-
-if not ok then
-	hl.dispatch(hl.dsp.exec_cmd('nofify-send "Warning" "monitors.lua is missing."'))
-end
