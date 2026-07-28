bc666d1750941e65cb464255f0a30990fb2b90c3
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-07-28 20:59:29 +0800

    remove qt packages

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -15,10 +15,6 @@
     corefonts
     xdg-desktop-portal-gtk
     xdg-desktop-portal
-    libsForQt5.qtstyleplugin-kvantum
-    libsForQt5.qt5ct
-    qt6Packages.qtstyleplugin-kvantum
-    qt6Packages.qt6ct
     wayland-logout
     blueman
     pavucontrol
