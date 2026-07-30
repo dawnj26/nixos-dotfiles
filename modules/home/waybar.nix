f57a178a50b5991b3ec2d5f4fe4efef6797fceb4
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-07-30 20:34:38 +0800

    move waybar package

--- modules/home/waybar.nix
+++ modules/home/waybar.nix
@@ -1,3 +1,11 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    waybar
+  ];
+
   xdg.configFile."waybar".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-dotfiles/config/waybar";
 }
