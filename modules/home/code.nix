9a4b0ce1fd2116376b303cffcbd3ae27cb1c4d82
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-08-01 13:31:45 +0800

    use zeditor bin

--- modules/home/code.nix
+++ modules/home/code.nix
@@ -1,7 +1,18 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  system = pkgs.stdenv.hostPlatform.system;
+in {
   programs.vscode = {
     enable = true;
     mutableExtensionsDir = true;
     package = pkgs.vscode.fhs;
+  };
+
+  programs.zed-editor = {
+    enable = true;
+    package = inputs.self.packages.${system}.zed-editor-bin;
   };
 }
