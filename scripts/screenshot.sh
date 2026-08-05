4d3b446cc0d4498b2c8cce4626478783ab14c76f
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-08-06 02:57:42 +0800

    use wl-copy

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -7,5 +7,5 @@ mkdir -p "$DIR"
 
 FILE="$DIR/Screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png"
 
-grim -g "$(slurp -o -r -c '#ff0000ff')" -t ppm - | satty --filename - --fullscreen --output-filename "$FILE"
+grim -g "$(slurp -o -r -c '#ff0000ff')" -t ppm - | satty --filename - --fullscreen --output-filename "$FILE" --copy-command wl-copy
 
