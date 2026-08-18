d8ecddace59fa9027990b0b7829bc6592e751e46
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-08-19 00:01:36 +0800

    revert helium package

--- modules/home/browser.nix
+++ modules/home/browser.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{
   home.file.".config/chromium-flags.conf".text = ''
     --ozone-platform-hint=auto
     --ozone-platform=wayland
@@ -16,12 +16,5 @@
       "--enable-gpu-rasterization"
       "--enable-zero-copy"
     ];
-    package = pkgs.helium.overrideAttrs (_: rec {
-      version = "0.15.4.1";
-      src = pkgs.fetchurl {
-        url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-bin_${version}-1_amd64.deb";
-        sha256 = "sha256-p10p9QFsZS0D/6oyPrSFV2UYqHE75BbOa6/bGzcE0Ag=";
-      };
-    });
   };
 }
