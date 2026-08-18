2cea935fd4e0832adf8e747cc9938dd1985e0bdf
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-08-18 23:56:53 +0800

    hide noctalia logo

--- modules/system/display-manager.nix
+++ modules/system/display-manager.nix
@@ -2,11 +2,11 @@
   programs.noctalia-greeter = {
     enable = true;
 
-    # Optional configuration
     greeter-args = "";
-    # Full declarative greeter.toml (overwritten on each activation).
-    # See examples/greeter.toml for every key (appearance.palette, output, …).
     settings = {
+      appearance = {
+        hide_logo = true;
+      };
       cursor = {
         theme = "Bibata-Modern-Ice";
         size = 24;
