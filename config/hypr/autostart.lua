4f4f46393b1579623822df7d3d2ba97b414e0c10
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-08-18 22:36:22 +0800

    remove waybar and hyprpaper

--- config/hypr/autostart.lua
+++ config/hypr/autostart.lua
@@ -3,8 +3,6 @@ hl.on("hyprland.start", function()
 
 	hl.exec_cmd("systemctl --user import-environment $(env | cut -d'=' -f 1)")
 	hl.exec_cmd("dbus-update-activation-environment --systemd --all")
-	hl.exec_cmd(utils.uwsm("hyprpaper"))
-	hl.exec_cmd(utils.uwsm("waybar"))
 	hl.exec_cmd(utils.uwsm("wl-paste --watch cliphist store"))
 	-- hl.exec_cmd(utils.uwsm("vesktop -m"))
 end)
