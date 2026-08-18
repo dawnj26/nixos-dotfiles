6d9b636404746522dca5e35a1b6a7bc668b0dd1f
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-08-18 23:58:45 +0800

    remove sddm

--- modules/system/security.nix
+++ modules/system/security.nix
@@ -1,8 +1,7 @@
-{...}: {
+{
   security = {
     rtkit.enable = true;
     polkit.enable = true;
     pam.services.login.enableGnomeKeyring = true;
-    pam.services.sddm.enableGnomeKeyring = true;
   };
 }
