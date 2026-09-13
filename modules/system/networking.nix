f3377f71822e02412ccd1d0772ca1bd6470e9c5e
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-09-13 10:31:05 +0800

    add default dns

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,7 +1,20 @@
-{...}: {
+{
   networking = {
     wireless.enable = true;
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      dns = "none";
+    };
+
+    useDHCP = false;
+    dhcpcd.enable = false;
+
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "8.8.8.8"
+      "8.8.4.4"
+    ];
 
     firewall = {
       enable = true;
