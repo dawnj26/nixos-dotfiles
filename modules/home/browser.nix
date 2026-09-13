9165605a2be9ab114a52d03a98fba783be998cac
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-09-13 10:30:29 +0800

    use newer version of helium

--- modules/home/browser.nix
+++ modules/home/browser.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   home.file.".config/chromium-flags.conf".text = ''
     --ozone-platform-hint=auto
     --ozone-platform=wayland
@@ -16,5 +16,16 @@
       "--enable-gpu-rasterization"
       "--enable-zero-copy"
     ];
+    package = pkgs.helium.overrideAttrs (_: rec {
+      version = "0.17.0.1";
+      src = pkgs.fetchurl {
+        url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-bin_${version}-1_amd64.deb";
+        sha256 = "sha256-mFj3RECYtEGkh4Zij1NTG3mWHJV8XUnrP0gX1qsc0MI=";
+      };
+    });
   };
+
+  home.packages = with pkgs; [
+    nur.repos.ymstnt.brave-origin
+  ];
 }
