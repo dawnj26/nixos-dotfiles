dc85e01da6e1fd0eaf73ff9a28561624bf3059b8
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-09-23 14:26:25 +0800

    update

--- modules/home/browser.nix
+++ modules/home/browser.nix
@@ -17,10 +17,10 @@
       "--enable-zero-copy"
     ];
     package = pkgs.helium.overrideAttrs (_: rec {
-      version = "0.17.0.1";
+      version = "0.17.2.1";
       src = pkgs.fetchurl {
         url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-bin_${version}-1_amd64.deb";
-        sha256 = "sha256-mFj3RECYtEGkh4Zij1NTG3mWHJV8XUnrP0gX1qsc0MI=";
+        sha256 = "sha256-xb4AhHoTY/AE+B07jnDKJmsVrgKgKdLLHhG2TThTaSk=";
       };
     });
   };
