926d666360608583169543dcdeb6b37a90d14be2
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-09-24 10:16:58 +0800

    add noctalia as trusted substituters

--- modules/system/settings.nix
+++ modules/system/settings.nix
@@ -6,7 +6,7 @@
 
   nix.settings = {
     substituters = ["https://attic.xuyh0120.win/lantian" "https://hyprland.cachix.org" "https://noctalia.cachix.org"];
-    trusted-substituters = ["https://hydra.nixos.org/" "https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hydra.nixos.org/" "https://hyprland.cachix.org" "https://noctalia.cachix.org"];
     trusted-public-keys = ["lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
     trusted-users = ["root" "@wheel"];
   };
